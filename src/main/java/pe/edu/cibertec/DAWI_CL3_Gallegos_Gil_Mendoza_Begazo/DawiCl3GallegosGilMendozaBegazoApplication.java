1b6a66c73ff9a0f884d3dbe8526675418b4d7ac5
AnaGil27 <gilyalta27@gmail.com>
2023-06-23 11:34:08 -0500

    Repo Creado

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL3_Gallegos_Gil_Mendoza_Begazo/DawiCl3GallegosGilMendozaBegazoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL3_Gallegos_Gil_Mendoza_Begazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3GallegosGilMendozaBegazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3GallegosGilMendozaBegazoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWI_CL3_Gallegos_Gil_Mendoza_Begazo/DawiCl3GallegosGilMendozaBegazoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL3_Gallegos_Gil_Mendoza_Begazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3GallegosGilMendozaBegazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
